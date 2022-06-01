1381d25f2adc21f04d52d110582f10af682591fe
Roman Valko <rvalko.dev@gmail.com>
2022-06-01 23:43:08 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 const galleryRef = document.querySelector('.gallery')
-galleryRef.addEventListener('click', lockLink)
-galleryRef.addEventListener('click', getRefImage)
+
+galleryRef.addEventListener('click', lockLinkWorks)
+galleryRef.addEventListener('click', openModalPicture)
+
 
 const gallaryBlocks = galleryItems.map(({preview, original, description} = {}) => {
       return `<div class="gallery__item">
@@ -19,24 +20,30 @@ const gallaryBlocks = galleryItems.map(({preview, original, description} = {}) =
 }).join("")
 galleryRef.insertAdjacentHTML('afterbegin', gallaryBlocks)
 
-function lockLink(event) {
+function lockLinkWorks(event) {
   event.preventDefault();
 }
 
-function getRefImage(event) {
-  if (!event.target.classList.contains('gallery__image')) {
-    return;
-  }
-  openModalWindow(event);
-}
-
-function openModalWindow(event) {
-  const lightBoxModal = basicLightbox.create(`<img src="${event.target.dataset.source}" alt="${event.target.alt}">`)
-  lightBoxModal.show()
-  window.addEventListener('keydown', closeOnKeyEscape)
-  function closeOnKeyEscape(event) {
-      if(event.code === "Escape"){
-        lightBoxModal.close(() => window.removeEventListener('keydown', closeOnKeyEscape))
-      }
+function openModalPicture(event) {
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}" alt="${event.target.alt}">
+    </div>
+`)
+  instance.show(() => {
+    window.addEventListener('keydown', closeModalOnEscape)
+    const modal = document.querySelector('.basicLightbox--visible')
+    modal.addEventListener('click', closeModalOnClick)
+  })
+  
+  function closeModalOnEscape(event) {
+    if (event.code == "Escape") {
+      instance.close(() => window.removeEventListener('keydown', closeModalOnEscape))
     }
+  }
+  function closeModalOnClick(event) {
+    if (event.target.className == "basicLightbox--visible") {
+      instance.close(() => window.removeEventListener('keydown', closeModalOnEscape))
+    }
+  }
 }
