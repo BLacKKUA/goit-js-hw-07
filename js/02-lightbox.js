99061e3c034626dc4082ea1383fcdfa920acd199
Roman Valko <rvalko.dev@gmail.com>
2022-06-04 01:24:03 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const test = document.querySelector('.gallery')
+const galleryRef = document.querySelector('.gallery')
 
 const gallaryBlocks = galleryItems.map(({preview, original, description} = {}) => {
       return `<div class="gallery__item">
@@ -14,6 +14,5 @@ const gallaryBlocks = galleryItems.map(({preview, original, description} = {}) =
   </a>
 </div>`
 }).join("")
-test.insertAdjacentHTML('afterbegin', gallaryBlocks)
-
-let gallery = new SimpleLightbox('.gallery a');
+galleryRef.insertAdjacentHTML('afterbegin', gallaryBlocks)
+let gallery = new SimpleLightbox('.gallery__link', {captionsData: "alt", captionDelay:"250"});
